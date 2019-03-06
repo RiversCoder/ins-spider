22d23336840551b56d1fd7ec1785405a0bbd7bdd
李广川 <testman@hotmail.com>
2019-03-06 17:10:38 +0800

    new

--- instagram.py
+++ instagram.py
@@ -11,6 +11,48 @@ import socket
 import socks
 
 
+# https://wallpaper.mob.org/image/downloadImage?id=40703&l=240&t=0&r=468&b=415&s=0.5932475884244373
+
+"""
+id: 40703
+l: 137
+t: 0
+r: 365
+b: 415
+s: 0.5928571428571429
+
+id: 47437
+l: 132
+t: 0
+r: 360
+b: 415
+s: 0.5937834941050375
+
+
+# 壁纸爬虫网站 分类有
+    
+    ABSTRACT 475张 ANIMALS 633张 ART 464张 CARS 451张 FOOD & DRINK 285张 GAMES 267张 MOVIES 356张
+    
+    MUSIC 116张 NATURE 1993张 PHOTOS 829张 PLACES 411张 QUOTES 66张 SPORTS 127张
+    
+
+http://www.mobileswall.com/#
+
+http://www.mobileswall.com/wallpaper/swan-2/
+http://www.mobileswall.com/wp-content/uploads/2015/12/901-Swan-l.jpg
+http://www.mobileswall.com/wallpaper/good-scenery-come-out-to-find-something-to-eat/
+http://www.mobileswall.com/wp-content/uploads/2015/12/901-Good-Scenery-Come-Out-to-Find-Something-to-Eat-l.jpg
+
+http://www.mobileswall.com/wallpaper/want-to-eat-something/
+http://www.mobileswall.com/wp-content/uploads/2015/12/901-Want-to-Eat-Something-l.jpg
+
+http://www.mobileswall.com/wp-content/uploads/2015/07/901-Red-whiskered-Bulbuls-l.jpg
+
+
+http://www.mobileswall.com/wp-content/uploads/2015/07/300-Ripples-l.jpg
+http://www.mobileswall.com/wp-content/uploads/2015/07/901-Ripples-l.jpg
+"""
+
 # 境外连接socks
 ip='127.0.0.1' # change your proxy's ip
 port = 10801 # change your proxy's port
@@ -21,7 +63,7 @@ url_base = u'https://www.instagram.com/'
 uri = u'https://www.instagram.com/graphql/query/?query_hash=a5164aed103f24b03e7b7747a2d94e3c&variables=%7B%22id%22%3A%22{user_id}%22%2C%22first%22%3A12%2C%22after%22%3A%22{cursor}%22%7D'
 
 
-BASE_PATH = 'C:\\pictures\\ins\\'
+BASE_PATH = 'D:\\videos\\instagram\\'
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
@@ -51,7 +93,7 @@ def get_json(url):
             print('请求网页json错误, 错误状态码：', response.status_code)
     except Exception as e:
         print(e)
-        time.sleep(20 + float(random.randint(1, 4000))/100)
+        time.sleep(5 + float(random.randint(1, 4000))/100)
         return get_json(url)
 
 
@@ -119,7 +161,7 @@ def main(user):
         print('\n正在下载第{0}张： '.format(i) + urls[i], ' 还剩{0}张'.format(len(urls)-i-1))
         try:
             content = get_content(urls[i])
-            file_path = BASE_PATH+'{0}\\{1}.{2}'.format(user, md5(content).hexdigest(), urls[i][-43:-40])
+            file_path = BASE_PATH+'{0}\\{1}.{2}'.format(user, '高质量-韩国-模特-生活-'+str(i), urls[i][-43:-40]) #md5(content).hexdigest()
             if not os.path.exists(file_path):
                 with open(file_path, 'wb') as f:
                     print('第{0}张下载完成： '.format(i) + urls[i])
